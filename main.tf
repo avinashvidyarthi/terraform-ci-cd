276012d7c198223f9e02dd0eaf84a52f68773bbd
Avinash Kumar <avinashvidyarthi@gmail.com>
2023-01-19 00:03:28 +0530

    changed machine type

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
 
 resource "google_compute_instance" "example" {
   name = "my-vm"
-  machine_type = "n1-standard-1"
+  machine_type = "f1-micro"
   zone = "us-central1-a"
   boot_disk {
     initialize_params {
