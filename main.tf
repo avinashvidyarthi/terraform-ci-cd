268c8a4cdc5d33642b7078201b31cb12e31800bb
Avinash Kumar <avinashvidyarthi@gmail.com>
2023-01-18 23:04:17 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "google" {
+  # credentials = file("./key_editor.json")
+  project = "avinashvidyarthi"
+  region = "us-central1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "do-whatever-you-want-here"
+    prefix = "terraform/"
+  }
+}
+
+resource "google_compute_instance" "example" {
+  name = "my-vm"
+  machine_type = "f1-micro"
+  zone = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+  scheduling {
+    automatic_restart = false
+    on_host_maintenance = "TERMINATE"
+  }
+}
